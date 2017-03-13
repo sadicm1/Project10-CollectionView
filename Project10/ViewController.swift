310519798d526b0905e686dae21173ba7da0cf9b
Mehmet Sadıç <mehmetsadic@Mehmet-iMac.local>
2017-03-14 00:46:15 +0300

    Ask which action to take before renaming the person alert. Use camera

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+  // Store all the person info into people array
   var people = [Person]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Add a bar button item to the left of navigation bar
     navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(selectImage))
   }
   
+  // Define the number of items in collection view
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    // Number of items in people array is equal to number of items in collection view
     return people.count
   }
   
+  // Assign image and name of the item cell
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
     
@@ -34,26 +39,51 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     return cell
   }
   
+  // When the image selection is finished run the code below
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
     
+    // Choose an unique name for the image chosen
     let imageName = UUID().uuidString
+    
+    // Find the path of image to save
     let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
     
+    // Transform image to image data and save it to image path
     if let imageData = UIImageJPEGRepresentation(image, 80) {
       try? imageData.write(to: imagePath)
     }
     
+    // Create a Person instance called person. The name is unknown and will be defined later
     let person = Person(name: "unknown", image: imageName)
     people.append(person)
+    
+    // We should reload our data so the the changes are reflected.
     collectionView?.reloadData()
     
+    // dismiss the view controller which was previously selected.
     dismiss(animated: true)
   }
   
+  // Run this code when the image is tapped.
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let person = people[indexPath.item]
     
+    let ac = UIAlertController(title: "Choose the action to proceed", message: nil, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Delete Image", style: .default) { [unowned self] _ in
+      self.people.remove(at: indexPath.item)
+      self.collectionView?.reloadData()
+    })
+    ac.addAction(UIAlertAction(title: "Rename Person", style: .default) { [unowned self] _ in
+      self.rename(person)
+    })
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(ac, animated: true)
+
+
+  }
+  
+  private func rename(_ person: Person) {
     let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
     ac.addTextField()
     ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
@@ -77,6 +107,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     let picker = UIImagePickerController()
     picker.delegate = self
     picker.allowsEditing = true
+    picker.sourceType = .camera
     present(picker, animated: true)
   }
 
